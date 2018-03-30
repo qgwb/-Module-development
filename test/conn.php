ca324c12eb684a1f116904944663dae86dd66273
HPYAEyes <32727441+HPYAEyes@users.noreply.github.com>
2018-03-30 21:24:34 +0800

    update

---
+++ test/conn.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/3/27 0027
+ * Time: 下午 5:11
+ */
+$servername = "localhost";
+$username = "root";
+$password = "123456";
+$database = "hpy";
+
+$db=mysqli_connect($servername,$username,$password,$database);
+if(mysqli_connect_errno()){
+    echo "ERROR connect";
+    exit;
+}
+//else{
+//    echo "Connect Success!";
+//}
+$typeid = $_GET['typeid'];
+//$typeid = 2001;
+$result = mysqli_query($db,"select * from typedetail where Type_id = ".$typeid);
+$rownum=mysqli_num_rows($result);
+echo '<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="jquery-weui-build/dist/lib/weui.min.css">
+		<link rel="stylesheet" type="text/css" href="jquery-weui-build/dist/css/jquery-weui.min.css">
+		</head>
+	    <body ontouchstart>
+	    <div id="maingrids">
+    			<div class="weui-grids">';
+
+for($i=0;$i<$rownum;$i++) {
+    $row = mysqli_fetch_assoc($result);
+    echo '<a href="typedetail.php?typedetailid=' . $row['Typedetail_id'] . '" class="weui-grid js_grid">
+        			<div class="weui-grid__icon">
+          				<img src="img/' . $row['Typedetail_id'] . '.png" alt="">
+        			</div>
+        			<p class="weui-grid__label">' . $row['Typedetail_name'] . '</p>
+      				</a>';
+}
+echo '</div>
+    		</div>
+		<script src="jquery-weui-build/dist/lib/jquery-2.1.4.js"></script>
+		<script src="jquery-weui-build/dist/js/jquery-weui.min.js"></script>
+		<script src="jquery-weui-build/dist/js/swiper.min.js"></script>
+		<script src="jquery-weui-build/dist/lib/fastclick.js"></script>
+		<script>
+  			$(function() {
+    			FastClick.attach(document.body);
+  			});
+
+		</script>';
+?>
